a2ffd4e29a1a3427972eabe4da22e4415e4221a9
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2020-07-22 14:56:37 +0300

    Update login.php

--- mc-admin/login.php
+++ mc-admin/login.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" type="text/css" href="css/login.css">
   <div class="login-wrap">
 	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Giriş Yap</label>
-		<input id="tab-2" type="radio" name="tab" class="for-pwd"><label for="tab-2" class="tab">Şifremi Unuttum</label>
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked/><label for="tab-1" class="tab">Giriş Yap</label>
+		<input id="tab-2" type="radio" name="tab" class="for-pwd"/><label for="tab-2" class="tab">Şifremi Unuttum</label>
 		<div class="login-form">
 			<div class="sign-in-htm">
 				<form action="operations/login.php" method="POST">
