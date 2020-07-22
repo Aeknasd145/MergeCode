c9e889bdf1081ca32e769d03ca156425a1a767b0
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2020-07-22 14:57:19 +0300

    Update login.php

--- mc-admin/login.php
+++ mc-admin/login.php
@@ -1,6 +1,9 @@
+<?php include("connect.php") ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.0.3.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="login.js"></script>
+<script type="text/javascript">$(function(){});</script>
 <link rel="stylesheet" type="text/css" href="css/login.css">
   <div class="login-wrap">
 	<div class="login-html">
@@ -8,7 +11,10 @@
 		<input id="tab-2" type="radio" name="tab" class="for-pwd"/><label for="tab-2" class="tab">Şifremi Unuttum</label>
 		<div class="login-form">
 			<div class="sign-in-htm">
-				<form action="operations/login.php" method="POST">
+				<form id="frm">
+					<div class="col-12" style="text-align: center;">
+	        			<div id="sonuc"></div><br />
+	        		</div>
 					<div class="group">
 						<label for="user" class="label">E-Mail</label>
 						<input name="email" id="user" type="text" class="input"/>
@@ -18,15 +24,15 @@
 						<input name="pass" id="pass" type="password" class="input" data-type="password"/>
 					</div>
 					<div class="group">
-						<input type="submit" class="button" value="Sign In"/>
+						<input type="button" id="btn" class="button" value="Sign In">
 					</div>
 				</form>
 				<div class="hr"></div>
 			</div>
 			<div class="for-pwd-htm">
 				<div class="group">
-					<label for="user" class="label">Username or Email</label>
-					<input id="user" type="text" class="input"/>
+					<label for="user" class="label">E-Mail</label>
+					<input id="user" name="email" type="email" class="input"/>
 				</div>
 				<div class="group">
 					<input type="submit" class="button" value="Reset Password"/>
