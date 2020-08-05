ba740fb3a746b3977becfefbed700a65e8518583
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2020-08-05 18:08:52 +0300

    Delete about-us.js

--- mc-admin/about-us.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("#btn").click(function(){var veri=$("#frm").serialize();$.ajax({type:"post",url:"operations/save-about-us.php",data:veri,success:function(sonuc){$("#sonuc").html((sonuc));}});});});
